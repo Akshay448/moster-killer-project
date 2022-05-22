608a0a7e5a311945ea21158962000c4c0b545c15
Akshay448 <akshaykapoor448@gmail.com>
2022-05-22 16:54:12 +0530

    adding console log and alert based on if else

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -8,8 +9,20 @@ adjustHealthBars(chosenMaxLife);
 
 function attackHandler() {
   const damage = dealMonsterDamage(ATTACK_VALUE);
-  console.log(damage);
+  console.log("damage to monster " + damage);
   currentMonsterHealth -= damage;
+  console.log("currentMonsterHealth " + currentMonsterHealth);
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  console.log("damage to player " + playerDamage);
+  currentPlayerHealth -= playerDamage;
+  console.log("currentPlayerHealth " + currentPlayerHealth);
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('You won!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('You lost!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert('You have a draw!');
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler);
